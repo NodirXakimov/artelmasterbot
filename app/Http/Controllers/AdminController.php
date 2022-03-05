5a675453709fab680ef357501ece181c693ad7bd
Nodir <nodirxakimov@yandex.ru>
2022-03-05 10:37:30 +0500

    Getting all users instead of paginating

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,7 +27,7 @@ class AdminController extends Controller
      */
     public function subscribers()
     {
-        $subscribers = Chat::paginate();
+        $subscribers = Chat::all();
 //        return response()->json($subscribers);
         return view('admin.subscribers', ['subscribers' => $subscribers]);
     }
